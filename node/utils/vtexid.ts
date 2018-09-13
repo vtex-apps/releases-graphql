8f112d4a730f8662b787d3a9ed8616ba1e7d013b
Guilherme Rodrigues <gadr90@gmail.com>
2018-09-13 09:09:19 -0300

    Create function once

--- node/utils/vtexid.ts
+++ node/utils/vtexid.ts
@@ -1,8 +1,9 @@
 import { parse } from 'cookie'
 import { head, pickBy, values} from 'ramda'
 
+const startsWithVtexId = (_, key) => key.startsWith('VtexIdclientAutCookie')
+
 export const getTokenFromCookies = (cookieHeader: string): string => {
   const cookies = parse(cookieHeader)
-  const startsWithVtexId = (_, key) => key.startsWith('VtexIdclientAutCookie')
   return head(values(pickBy(startsWithVtexId, cookies)))
 }
